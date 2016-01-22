559be429f1758fe693a1e81e6c431c027fe479e3
Дмитрий Мурзин <dm1998@list.ru>
2016-01-22 19:42:05 +0300

    fix 3

--- my.user.js
+++ my.user.js
@@ -10,35 +10,66 @@
 // @run-at       document-body
 // ==/UserScript==
 
-function miniMapInit() {
+(function() {
 	var $ = window.jQuery;
+	var render_timer = null;
 	
-	// minimap dom
-	if ($('#mini-map-wrapper').length === 0){
-	var wrapper = $('<div>').attr('id', 'mini-map-wrapper').css({
-		position: 'fixed',
-		bottom: 50,
-		right: 10,
-		width: 300,
-		height: 300,
-		background: 'rgba(128, 128, 128, 0.58)'
+	function miniMapRender() {
+		var canvas = window.mini_map;
+		var ctx = canvas.getContext('2d');
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		
+            var x = 0.5 * canvas.width;
+            var y = 0.5 * canvas.height;
+            var size = 0.1 * canvas.width;
+
+            ctx.beginPath();
+            ctx.arc(
+                x,
+                y,
+                size,
+                0,
+                2 * Math.PI,
+                false
+            );
+            ctx.closePath();
+            ctx.fillStyle = token.color;
+            ctx.fill();
+	}
+	
+	function miniMapInit() {
+		// minimap dom
+		if ($('#mini-map-wrapper').length === 0) {
+			var wrapper = $('<div>').attr('id', 'mini-map-wrapper').css({
+				position: 'fixed',
+				bottom: 50,
+				right: 10,
+				width: 300,
+				height: 300,
+				background: 'rgba(128, 128, 128, 0.58)'
+			});
+
+			var mini_map = $('<canvas>').attr({
+				id: 'mini-map',
+				width: 300,
+				height: 300
+			}).css({
+				width: '100%',
+				height: '100%',
+				position: 'relative'
+			});
+
+			wrapper.append(mini_map).appendTo(document.body);
+
+			window.mini_map = mini_map[0];
+		}
+
+		// minimap renderer
+		if (render_timer === null)
+			render_timer = setInterval(miniMapRender, 1000 / 30);
+	}
+	
+	$(window.document).ready(function() {
+		miniMapInit();
 	});
-
-	var mini_map = $('<canvas>').attr({
-		id: 'mini-map',
-		width: 300,
-		height: 300
-	}).css({
-		width: '100%',
-		height: '100%',
-		position: 'relative'
-	});
-
-	wrapper.append(mini_map).appendTo(document.body);
-
-	window.mini_map = mini_map[0];
-}
-
-$(window.document).ready(function() {
-	miniMapInit();
-});
+})();
