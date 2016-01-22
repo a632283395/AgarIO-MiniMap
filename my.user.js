c0d7f9340058ed415ef4e027068b2e8fefa6ad45
Дмитрий Мурзин <dm1998@list.ru>
2016-01-22 21:26:22 +0300

    fix 5

--- my.user.js
+++ my.user.js
@@ -14,27 +14,47 @@
 	var $ = window.jQuery;
 	var render_timer = null;
 	
+	window.WebSocket = function(url, protocols) {
+		console.log('Listen');
+	}
+	
 	function miniMapRender() {
 		var canvas = window.mini_map;
 		var ctx = canvas.getContext('2d');
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		var w = canvas.width;
+		var h = canvas.height;
 		
-            var x = 0.5 * canvas.width;
-            var y = 0.5 * canvas.height;
-            var size = 0.1 * canvas.width;
-
-            ctx.beginPath();
-            ctx.arc(
-                x,
-                y,
-                size,
-                0,
-                2 * Math.PI,
-                false
-            );
-            ctx.closePath();
-            ctx.fillStyle = 'green';
-            ctx.fill();
+		var x = 0.5 * w;
+		var y = 0.5 * h;
+		var size = 0.1 * w;
+		
+		ctx.beginPath();
+		ctx.arc(
+			x,
+			y,
+			size,
+			0,
+			2 * Math.PI,
+			false
+		);
+		ctx.closePath();
+		ctx.fillStyle = '#00ffff';
+		ctx.fill();
+		
+		ctx.strokeStyle = 'green';
+		drawLine(ctx, 0, 0, w, h);
+		drawLine(ctx, 0, h, w, 0);
+		ctx.strokeStyle = 'yellow';
+		drawLine(ctx, w/2, 0, w/2, h);
+		drawLine(ctx, 0, h/2, w, h/2);
+	}
+	
+	function drawLine(canvas, x0, y0, x1, y1) {
+		canvas.beginPath();
+		canvas.moveTo(x0, y0);
+		canvas.lineTo(x1, y1);
+		canvas.stroke();
 	}
 	
 	function miniMapInit() {
@@ -42,8 +62,8 @@
 		if ($('#mini-map-wrapper').length === 0) {
 			var wrapper = $('<div>').attr('id', 'mini-map-wrapper').css({
 				position: 'fixed',
-				bottom: 50,
-				right: 10,
+				bottom: 20,
+				right: 20,
 				width: 300,
 				height: 300,
 				background: 'rgba(128, 128, 128, 0.58)'
